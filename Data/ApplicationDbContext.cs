930fe59375df7919b26ab5a81a8d29d0481d51b0
Hussam-Alkhalaf <hussam.alkhalaf@web.de>
2025-03-28 08:54:00 +0100

    Projektdateien hinzufügen.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CMS.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMS.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<JobPostingModel> jobPostings { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Models/JobPostingModel.cs
@@ -0,0 +1,19 @@
+﻿namespace CMS.Models
+{
+    public class JobPostingModel
+    {
+        public int Id { get; set; }
+        public string JobTitle { get; set; }
+        public string JobDescription { get; set; }
+        public string JobLocation { get; set; }
+        public DateTime StartDate { get; set; }
+        public int Salary { get; set; }
+        public string CampanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactMail { get; set; }
+        public string CompanyWebsite { get; set; }
+        public byte[] CompanyImage { get; set; }
+
+
+    }
+}
